3a06ed16f5ced9394df458f5b46dc9dd3e3b6b84
IlkayAtes11 <102388251+IlkayAtes11@users.noreply.github.com>
2022-12-14 13:16:08 -0800

    update

--- code/query.sql
+++ code/query.sql
@@ -51,7 +51,7 @@ de.dept_no=d.dept_no
 WHERE dept_name = 'Sales' or dept_name = 'Development'
 
 -- List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
-SELECT last_name, COUNT(last_name)
+SELECT last_name, COUNT(last_name) AS last_name_count
 FROM employees
 GROUP BY last_name
-ORDER BY COUNT(last_name) DESC
+ORDER BY last_name_count DESC
